da60908ced80b8d1e55cebfa0a0a0c363ec6f5e8
Sebastian Straburzynski <sebastian@straburzynski.pl>
2024-01-31 17:23:58 +0100

    Controls Component - white and black keys.

--- src/components/ControlsComponent/ControlsComponent.tsx
+++ src/components/ControlsComponent/ControlsComponent.tsx
@@ -119,7 +119,7 @@ const ControlsComponent: FC<ControlsComponentProps> = ({ onHandleKey }) => {
                             onMouseDown={(e) => onHandleKey(e, value)}
                             onMouseUp={(e) => onHandleKey(e, value)}
                         >
-                            <div className={`${styles.controlBtn} ${value}`}>
+                            <div className={`${styles.controlBtn} ${value} ${normalizeNotes(value).includes('#') ? styles.black : styles.white}`}>
                                 <p className={styles.label}>
                                     {key} - {normalizeNotes(value)}
                                 </p>
