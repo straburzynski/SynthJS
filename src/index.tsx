19f66a60c8dc9be90a5147d1938679b1c3c89e9f
Sebastian Straburzynski <sebastian@straburzynski.pl>
2024-02-06 18:05:20 +0100

    package.json update dependencies.

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
-import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createRoot, Root } from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container: HTMLElement = document.getElementById('root')!;
+const root: Root = createRoot(container);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
